2bc1d3efcb043135b606d76e20b46e3df02322f5
Emil Veldhuizen <Emil Veldhuizen>
2016-08-25 17:29:37 +0200

    - added "add to clipboard"

--- js/main.js
+++ js/main.js
@@ -39,3 +39,36 @@ $('#randomnick').click(function() {
 });
 
 $('textarea').html($('textarea').html().trim());
+
+// Tooltip
+
+$('#btn-copy').tooltip({
+    trigger: 'click',
+    placement: 'bottom'
+});
+
+function setTooltip(btn, message) {
+    $(btn).tooltip('hide')
+        .attr('data-original-title', message)
+        .tooltip('show');
+}
+
+function hideTooltip(btn) {
+    setTimeout(function() {
+        $(btn).tooltip('hide');
+    }, 1000);
+}
+
+// Clipboard
+
+var clipboard = new Clipboard('#btn-copy');
+
+clipboard.on('success', function(e) {
+    setTooltip(e.trigger, 'Copied!');
+    hideTooltip(e.trigger);
+});
+
+clipboard.on('error', function(e) {
+    setTooltip(e.trigger, 'Failed!');
+    hideTooltip(e.trigger);
+});
