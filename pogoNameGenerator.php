75524a3ed847dfd4835deafd16df6e6c0d8533f3
Emil Veldhuizen <Emil Veldhuizen>
2016-08-25 13:27:27 +0200

    - php formated & some minor name changes

--- pogoNameGenerator.php
+++ pogoNameGenerator.php
@@ -7,73 +7,82 @@ class pogoNameGenerator
     {
         $this->names = preg_split('/\n|\r/', $this->getPost('name'), -1, PREG_SPLIT_NO_EMPTY);
 
-        if($_SERVER['REQUEST_METHOD'] == 'POST')
-        {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $this->runGenerator();
         }
     }
 
     private function runGenerator()
     {
-        if ($this->getPost('randomnick') == 'yes')
-        {
-            $this->generateRandomNick($this->getPost('quantity-nick'));
+        if ($this->getPost('randomnick') == 'yes') {
+            $this->generateRandomNickname();
         }
-        if ($this->getPost('randomcheck') == 'yes')
-        {
+        if ($this->getPost('randomcheck') == 'yes') {
             $this->addRandomString();
         }
-        if ($this->getPost('passcheck') == 'yes')
-        {
+        if ($this->getPost('passcheck') == 'yes') {
             $this->addPassword();
         }
-        if ($this->getPost('comma') == 'yes')
-        {
+        if ($this->getPost('comma') == 'yes') {
             $this->addComma();
         }
     }
 
-    private function generateRandomNick($quantity)
+    private function generateRandomNickname()
     {
-        $random_nickname = file_get_contents('https://donjon.bin.sh/name/rpc.cgi?type='.$this->getNickType().'&n='.$quantity.'');
+        $types = array(
+            'Dwarvish+Male',
+            'Dwarvish+Female',
+            'Draconic+Male',
+            'Draconic+Female',
+            'Orcish+Male',
+            'Orcish+Female',
+            'Incan+Male',
+            'Incan+Female',
+            'Sioux+Male',
+            'Sioux+Female'
+        );
+        $random_type = array_rand($types, 1);
+        $random_nickname = file_get_contents('https://donjon.bin.sh/name/rpc.cgi?type=' .  $types[$random_type] . '&n=' . $this->getPost('quantity-nick') . '');
         $this->names = preg_split('/\n|\r/', $random_nickname, -1, PREG_SPLIT_NO_EMPTY);
     }
 
     private function addRandomString()
     {
-        $add_string = array_map(function($val) { return $val.$this->stringShuffle(); } , $this->names);
+        $add_string = array_map(function($val)
+        {
+            return $val . $this->generateRandomString($this->getPost('quantity-string'));
+        }, $this->names);
         $this->names = $add_string;
     }
 
     private function addPassword()
     {
-        $add_password = array_map(function($val) { return $val.':'.$this->getPost('password'); } , $this->names);
+        $add_password = array_map(function($val)
+        {
+            return $val . ':' . $this->getPost('password');
+        }, $this->names);
         $this->names = $add_password;
     }
 
     private function addComma()
     {
-        $add_comma = array_map(function($val) { return $val.','; } , $this->names);
+        $add_comma = array_map(function($val)
+        {
+            return $val . ',';
+        }, $this->names);
         $this->names = $add_comma;
     }
 
-    private function stringShuffle()
+    private function generateRandomString($quantity)
     {
-        $string_shuffle = substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, $this->getPost('quantity-string'));
+        $string_shuffle = substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, $quantity);
         return $string_shuffle;
-    }
-
-    private function getNickType()
-    {
-        $type = array('Celestial', 'Fiendish', 'Dwarvish+Male', 'Dwarvish+Female', 'Draconic+Male', 'Draconic+Female', 'Orcish+Male', 'Orcish+Female', 'Orcish+Town');
-        $random_type = array_rand($type, 1);
-        return $type[$random_type];
     }
 
     private function getPost($key, $default = null)
     {
-        if (isset($_POST[$key]))
-        {
+        if (isset($_POST[$key])) {
             return $_POST[$key];
         }
         return $default;
